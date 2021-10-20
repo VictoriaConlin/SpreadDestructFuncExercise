42d66672416f4c539050dd5d6119b6b50e4d97e4
Victoria Conlin <kinnevic@gmail.com>
2021-10-20 16:18:25 -0400

    Completed the initial part of the Spread Destructuring Functions Exercise

---
+++ script.js
@@ -0,0 +1,29 @@
+// Spread Destructuring Functions Exercise
+
+// 1a
+const jPDinos = [
+    `Velociraptors`,
+    `Tyrannosaurus Rex`,
+    `Dilophosaurus`
+  ];
+
+// 1b
+function seeDinos(a, ...b){
+    console.log(a);
+    console.log(b);
+  }
+  seeDinos(...jPDinos); 
+
+// 2a
+const jPCharacters = {
+    alanGrant: `Sam Neill`,
+    ellieSattler: `Laura Dern`,
+    ianMalcolm: `Goldblum`
+};
+
+// 2b
+seeCharacters = ({ellieSattler}) => {
+    console.log(ellieSattler);
+};
+
+seeCharacters(jPCharacters);
